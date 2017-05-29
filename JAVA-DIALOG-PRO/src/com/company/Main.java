aeedd46c3fe506031561d3f3601af5de9e782f84
kiransweng <kiransweng@gmail.com>
2017-05-29 07:23:08 +0530

    first commit for dialog pro

---
+++ JAVA-DIALOG-PRO/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+    private static JDialog d;
+
+    public static void main(String[] args) {
+	// write your code here
+        new Main();
+    }
+
+
+    Main() {
+        JFrame f= new JFrame();
+        d = new JDialog(f , "Dialog Example", true);
+        d.setLayout( new FlowLayout() );
+        JButton b = new JButton ("OK");
+        b.addActionListener ( new ActionListener()
+        {
+            public void actionPerformed( ActionEvent e )
+            {
+                Main.d.setVisible(false);
+            }
+        });
+        d.add( new JLabel ("Click button to continue."));
+        d.add(b);
+        d.setSize(400,400);
+        d.setVisible(true);
+    }
+
+}
